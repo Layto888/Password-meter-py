b6678469c73926184b5db45dd7177b068bcfb50f
Amine Amardjia <amardjia.amine@gmail.com>
2018-09-23 21:33:53 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from password_meter import Password
-from constants import *
-password = Password('Azerty22')
-password.rate()
-
-Password().find(8, display=True, spec=ONLY_PUNCTATIONS + ONLY_DIGITS)
