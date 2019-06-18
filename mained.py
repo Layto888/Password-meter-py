3ebba207e1f884d91032a0eede1a3cd50ced7d42
Layto888 <amardjia.amine@gmail.com>
2019-06-18 18:02:13 +0100

    comment edit

--- mained.py
+++ mained.py
@@ -2,7 +2,7 @@ from password_meter import Password
 from constants import *
 from matplotlib import pyplot as plt
 
-# find best password algorithm
+# algorithm to find best password 
 Password().find(12, spec=ALL, msg='spec=ALL')
 Password().find(12, spec=USE_LETTERS + USE_DIGITS, msg='spec=LETTERS + DIGITS')
 Password().find(12, spec=USE_LETTERS + USE_PUNCTATIONS, msg='spec=LETTERS + PUNCTATIONS')
